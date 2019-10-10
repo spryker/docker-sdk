25a4e15f1b38567ebd0eff3f5f329f768dbc73b4
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2019-10-10 16:51:55 +0300

    SC-2092: Adjust after CR

--- generator/index.php
+++ generator/index.php
@@ -131,15 +131,13 @@ file_put_contents(
     ])
 );
 
-if (isset($projectData['services']['swagger'])) {
-    file_put_contents(
-        $deploymentDir . DS . 'env' . DS . 'swagger.env',
-        $twig->render('env/swagger/swagger-ui.env.twig', [
-            'project' => $projectData,
-            'endpointMap' => $endpointMap,
-        ])
-    );
-}
+file_put_contents(
+    $deploymentDir . DS . 'env' . DS . 'swagger.env',
+    $twig->render('env/swagger/swagger-ui.env.twig', [
+        'project' => $projectData,
+        'endpointMap' => $endpointMap,
+    ])
+);
 
 file_put_contents(
     $deploymentDir . DS . 'docker-compose.yml',
