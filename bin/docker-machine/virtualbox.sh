882a5dbc495f1033ea073dc0c614157cea08c571
Filip Sushko <filip.sushko@spryker.com>
2019-12-06 13:03:41 +0200

    SC-1856: added configuration for docker-macnine

--- bin/docker-machine/virtualbox.sh
+++ bin/docker-machine/virtualbox.sh
@@ -13,10 +13,13 @@ popd > /dev/null
 function exportDockerMachineConfiguration()
 {
     export DOCKER_MACHINE_DRIVER=virtualbox
-    export DOCKER_MACHINE_NAME=${SPRYKER_DOCKER_PREFIX}-${SPRYKER_DOCKER_TAG}-${DOCKER_MACHINE_DRIVER}-machine
-    export DOCKER_MACHINE_CPU=2
-    export DOCKER_MACHINE_MEMORY=4096
-    export DOCKER_MACHINE_DISK_SIZE=20000
+
+    local defaultDockerMachineName=${SPRYKER_DOCKER_PREFIX}-${SPRYKER_DOCKER_TAG}-${DOCKER_MACHINE_DRIVER}-machine
+
+    export DOCKER_MACHINE_NAME=${DOCKER_MACHINE_NAME:-${defaultDockerMachineName}}
+    export DOCKER_MACHINE_CPU=${DOCKER_MACHINE_CPU:-2}
+    export DOCKER_MACHINE_MEMORY=${DOCKER_MACHINE_MEMORY:-4096}
+    export DOCKER_MACHINE_DISK_SIZE=${DOCKER_MACHINE_DISK_SIZE:-20000}
 }
 
 export -f exportDockerMachineConfiguration
