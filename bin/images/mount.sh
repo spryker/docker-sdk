0ad6ef867e652f5ef4933be12191a3bc4bbae320
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2020-01-17 13:12:39 +0200

    Adjust composer autoload for no-dev

--- bin/images/mount.sh
+++ bin/images/mount.sh
@@ -58,6 +58,8 @@ function buildBaseImages()
         --build-arg APPLICATION_ENV=${APPLICATION_ENV} \
         --build-arg SPRYKER_DB_ENGINE=${dbEngine} \
         --build-arg SPRYKER_LOG_DIRECTORY=${logDirectory} \
+        --build-arg SPRYKER_COMPOSER_MODE=${SPRYKER_COMPOSER_MODE} \
+        --build-arg SPRYKER_COMPOSER_AUTHOLOAD=${SPRYKER_COMPOSER_AUTOLOAD} \
         .
 
     doCliImage
