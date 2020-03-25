ea928482455499ec81c9aa3006fe86fefd3badf7
Mike Kalinin <mike.kalinin@spryker.com>
2020-03-25 10:36:18 +0200

    Adding New Relic. New database versions. Increasing wait time.

--- bin/database/mysql.sh
+++ bin/database/mysql.sh
@@ -8,7 +8,7 @@ popd > /dev/null
 
 function doesDatabaseHaveTables()
 {
-    tableCount=$(execSpryker 'VERBOSE=0 MYSQL_PWD="${SPRYKER_DB_ROOT_PASSWORD}" mysql -h ${SPRYKER_DB_HOST} -u ${SPRYKER_DB_ROOT_USERNAME} -e "SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = \"${SPRYKER_DB_DATABASE}\"" | wc -l |  sed "s/^ *//" || echo 0')
+    tableCount=$(execSpryker 'VERBOSE=0 MYSQL_PWD="${SPRYKER_DB_ROOT_PASSWORD}" mysql -h ${SPRYKER_DB_HOST} -u ${SPRYKER_DB_ROOT_USERNAME} -e "SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = \"${SPRYKER_DB_DATABASE}\"" | wc -l | sed "s/^ *//"  | tr -d " \n\r" || echo 0')
 
     [ "$tableCount" -gt 0 ] && return ${__TRUE} || return ${__FALSE}
 }
