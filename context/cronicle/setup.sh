2a6fc5d0deacbb6778b5eab372bc7cb5f44a2672
Filip Sushko <filip.sushko@spryker.com>
2021-06-16 19:46:55 +0300

    SC-7073: update console command

--- context/cronicle/setup.sh
+++ context/cronicle/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f "$DATA_DIR/.baked" ]; then
-    /data/vendor/bin/console cronicle:setup ${SPRYKER_CRONICLE_BASE_PATH} --install
+    /data/vendor/bin/console cronicle:install ${SPRYKER_CRONICLE_BASE_PATH} --install
 fi
 
 cd ${ROOT_DIR}
