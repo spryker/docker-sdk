3e238d593982591148403d0cecbedb0f69fbb49a
alexanderM91 <oleksandr.myrnyi@spryker.com>
2021-11-04 11:08:29 +0200

    Introduce a new GlueBackendApi endpoint

--- generator/index.php
+++ generator/index.php
@@ -132,6 +132,8 @@ const GLUE_APP = 'glue';
 const BACKOFFICE_APP = 'backoffice';
 const BACKEND_GATEWAY_APP = 'backend-gateway';
 const MERCHANT_PORTAL = 'merchant-portal';
+const GLUE_STOREFRONT = 'glue-storefront';
+const GLUE_BACKEND = 'glue-backend';
 
 const ENTRY_POINTS = [
     BACKOFFICE_APP => 'Backoffice',
@@ -140,6 +142,8 @@ const ENTRY_POINTS = [
     YVES_APP => 'Yves',
     GLUE_APP => 'Glue',
     MERCHANT_PORTAL => 'MerchantPortal',
+    GLUE_STOREFRONT => 'GlueStorefront',
+    GLUE_BACKEND => 'GlueBackend',
 ];
 
 foreach ($projectData['groups'] ?? [] as $groupName => $groupData) {
