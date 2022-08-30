5601eb5ec7e7c469c0d1fac007be328a14b5213c
Filip Sushko <filip.sushko@spryker.com>
2022-08-30 18:14:50 +0200

    SC-10366: fixed package compatibility in buster images

--- generator/index.php
+++ generator/index.php
@@ -155,7 +155,8 @@ const ENTRY_POINTS = [
     GLUE_BACKEND => 'GlueBackend',
 ];
 
-const DEBIAN_DISTRO_NAME = 'bullseye';
+const DEBIAN_BULLSEYE_DISTRO_NAME = 'bullseye';
+const DEBIAN_BUSTER_DISTRO_NAME = 'buster';
 const ALPINE_DISTRO_NAME = 'alpine';
 
 const SPRYKER_NODE_IMAGE_DISTRO_ENV_NAME = 'SPRYKER_NODE_IMAGE_DISTRO';
@@ -1624,8 +1625,12 @@ function buildNodeJsNpmBuildConfig(array $projectData): array
 function getNodeDistroName(array $nodejsConfig, string $imageName): string
 {
     if (array_key_exists('distro', $nodejsConfig)) {
-        if ($nodejsConfig['distro'] == 'debian') {
-            return DEBIAN_DISTRO_NAME;
+        if ($nodejsConfig['distro'] == 'bullseye') {
+            return DEBIAN_BULLSEYE_DISTRO_NAME;
+        }
+
+        if ($nodejsConfig['distro'] == 'buster') {
+            return DEBIAN_BUSTER_DISTRO_NAME;
         }
 
         if ($nodejsConfig['distro'] == 'alpine') {
@@ -1639,8 +1644,12 @@ function getNodeDistroName(array $nodejsConfig, string $imageName): string
         return DEFAULT_NODE_DISTRO;
     }
 
+    if (str_contains($imageData[1], 'debian-buster')) {
+        return DEBIAN_BUSTER_DISTRO_NAME;
+    }
+
     if (str_contains($imageData[1], 'debian')) {
-        return DEBIAN_DISTRO_NAME;
+        return DEBIAN_BULLSEYE_DISTRO_NAME;
     }
 
     return ALPINE_DISTRO_NAME;
