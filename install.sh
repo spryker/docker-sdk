65497ffdba66fa43f76288ba1e1d2c2ea531074b
Filip Sushko <filip.sushko@spryker.com>
2022-10-19 16:11:16 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
-
-set -x
+#!/bin/bash
 
 readonly DOCKER_SDK_PATH="${HOME}/.docker-sdk"
 readonly DOCKER_SDK_REPO="git@github.com:spryker/docker-sdk.git"
-readonly DOCKER_SDK_ALIAS="alias docker/sdk=${DOCKER_SDK_PATH}/sdk"
+readonly DOCKER_SDK_ALIAS="alias docker/sdk"
+readonly DOCKER_SDK_ALIAS_WITH_VALUE="${DOCKER_SDK_ALIAS}='${DOCKER_SDK_PATH}/sdk'"
 
 function cleanUpOlderVersion() {
   rm -rf "${DOCKER_SDK_PATH}"
@@ -34,12 +33,24 @@ function getProfileFile() {
   echo ${profilePath}
 }
 
+function registrationAlias() {
+  local profilePath=$(getProfileFile)
+  local isAliasExist=$(cat "${profilePath}" | grep "${DOCKER_SDK_ALIAS}")
+
+  if [ -z "${isAliasExist}" ]; then
+      echo "${DOCKER_SDK_ALIAS_WITH_VALUE}" >> "${profilePath}"
+      ${SHELL} -c "source ${profilePath}"
+  else
+    if [ "${isAliasExist}" != "${DOCKER_SDK_ALIAS_WITH_VALUE}" ]; then
+      printf "WARNING: Your 'docker/sdk' alias has wrong value. Please update your shell profile with next value: %s" "${DOCKER_SDK_ALIAS_WITH_VALUE}"
+    fi
+  fi
+}
+
 function install() {
   cleanUpOlderVersion
   clone
-  profilePath=$(getProfileFile)
-  echo "${DOCKER_SDK_ALIAS}" >> "${profilePath}"
-  source "${profilePath}"
+  registrationAlias
 }
 
 install
