19cda16709fbc280befc662e567b13cb90da9c3d
Filip Sushko <filip.sushko@spryker.com>
2022-11-25 16:35:47 +0100

    Updated help for `list-env` command

--- bin/command/env-configuration/list-env.sh
+++ bin/command/env-configuration/list-env.sh
@@ -2,7 +2,7 @@
 
 Registry::addCommand "list-env" "Command::list-env"
 
-Registry::Help::command -c "list-env" "Generate \`.env.docker.local file\` based on deploy file."
+Registry::Help::command -c "list-env" "Print env variables based on deploy file(environment-configuration section)."
 
 function Command::list-env() {
     if [ -f "${DEPLOYMENT_DIR}/.env.docker.list" ]; then
