692fe715b2295f816671fa80f025c8c7ad4738fe
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2023-03-08 11:03:35 +0100

    Update config.sh

--- bin/command/install/config.sh
+++ bin/command/install/config.sh
@@ -2,7 +2,7 @@
 
 Registry::addCommand "config" "Command::config"
 
-Registry::Help::command -s -c "config" "Output deploy file into the cli."
+Registry::Help::command -s -c "config" "Outputs deploy file into the cli."
 
 function Command::config() {
     shift $((OPTIND - 1))
