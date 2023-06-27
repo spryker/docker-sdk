a2f60faf5e4e754f9aaf531c2481e4bac969356a
alexanderM91 <oleksandr.myrnyi@spryker.com>
2023-06-27 10:58:00 +0200

    Adjust help command description

--- bin/command/compose/exec.sh
+++ bin/command/compose/exec.sh
@@ -2,7 +2,7 @@
 
 Registry::addCommand "exec" "Command::exec"
 
-Registry::Help::command -c "exec" -a "<service> <command>" "Execute a command in the specified ${HELP_HIGH}<service>${NC} that is SERVICE name shown in ${HELP_HIGH}${SELF_SCRIPT}docker/sdk ps${NC}"
+Registry::Help::command -c "exec" -a "<service> <command>" "Execute a command in the specified ${HELP_HIGH}<service>${NC} that is SERVICE name shown in ${HELP_HIGH}${SELF_SCRIPT} ps${NC}"
 
 function Command::exec() {
     Compose::performExec "${@}"
