80a718ca74676bb817d6488dbfc0164e301045fa
Vitalii Vatulia <vitalii.vatulia@spryker.com>
2024-01-05 16:59:07 +0100

    Added variable "JENKINS_MAX_TIME" to spryker.sh . Jenkins job can use it to configure max time.

--- context/jenkins/spryker.sh
+++ context/jenkins/spryker.sh
@@ -4,11 +4,20 @@ set -e
 
 echo "> ${COMMAND}"
 
+: "${JENKINS_MAX_TIME:=1000}"
+if [ "${JENKINS_MAX_TIME}" == "0" ]; then
+    MAX_TIME=''
+else
+    MAX_TIME="--max-time ${JENKINS_MAX_TIME}"
+fi
+
+echo "MAX_TIME = ${MAX_TIME}";
+
 http_response=$(curl --request POST -LsS \
     -o /dev/stderr \
     -w "%{http_code}" \
     --data "APPLICATION_STORE='${APPLICATION_STORE}' SPRYKER_CURRENT_REGION='${SPRYKER_CURRENT_REGION}' COMMAND='${COMMAND}' cli.sh" \
-    --max-time 1000 \
+    --max-time ${JENKINS_MAX_TIME} \
     --url "http://cli:9000/console")
 
 if [ "${http_response}" != "200" ]; then
