42a3aa351f2e3b5576ed6581a3b56d9e6a21c90f
Vitalii Vatulia <vitalii.vatulia@spryker.com>
2024-02-29 12:17:07 +0100

    Removed --max-time param in spryker.sh

--- context/jenkins/spryker.sh
+++ context/jenkins/spryker.sh
@@ -4,21 +4,21 @@ set -e
 
 echo "> ${COMMAND}"
 
-: "${JENKINS_MAX_TIME:=1000}"
-if [ "${JENKINS_MAX_TIME}" == "0" ]; then
-    MAX_TIME=''
-else
-    MAX_TIME="--max-time ${JENKINS_MAX_TIME}"
-fi
-
-echo "MAX_TIME = ${MAX_TIME}";
+#: "${JENKINS_MAX_TIME:=1000}"
+#if [ "${JENKINS_MAX_TIME}" == "0" ]; then
+#    MAX_TIME=''
+#else
+#    MAX_TIME="--max-time ${JENKINS_MAX_TIME}"
+#fi
+#
+#echo "MAX_TIME = ${MAX_TIME}";
 
 http_response=$(curl --request POST -LsS \
     -o /dev/stderr \
     -w "%{http_code}" \
     --data "APPLICATION_STORE='${APPLICATION_STORE}' SPRYKER_CURRENT_REGION='${SPRYKER_CURRENT_REGION}' COMMAND='${COMMAND}' cli.sh" \
-    --max-time ${JENKINS_MAX_TIME} \
     --url "http://cli:9000/console")
+#    ${MAX_TIME} \
 
 if [ "${http_response}" != "200" ]; then
     exit 1
