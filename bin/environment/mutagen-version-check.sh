3d5508f7b56f41a3268e964b2c048c503c4dd7ca
alexanderM91 <oleksandr.myrnyi@spryker.com>
2024-11-08 15:13:22 +0100

    Mutagen v0.18.* check

---
+++ bin/environment/mutagen-version-check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function Environment::checkMutagenVersion() {
+    local mutagenInstalledVersion="$(Mutagen::getInstalledVersion)"
+
+    if [[ "$mutagenInstalledVersion" == 0.18.* || "$mutagenInstalledVersion" > "0.18" ]]; then
+        Console::error "Error: Mutagen version 0.18.* is not supported yet. Please, run the command to install the latest supported version.
+brew unlink mutagen && brew unlink mutagen-compose && brew install mutagen-io/mutagen/mutagen@0.17 mutagen-io/mutagen/mutagen-compose@0.17"
+        exit 1
+    fi
+}
+
+function Mutagen::getInstalledVersion() {
+	echo $(String::trimWhitespaces "$(
+         command -v mutagen >/dev/null 2>&1
+         test $? -eq 0 && mutagen version
+     )")
+}
+
+Registry::addChecker "Environment::checkMutagenVersion"
