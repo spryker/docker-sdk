57d8c8ab40747601dc1825d9f5959f33a61741dd
alexanderM91 <oleksandr.myrnyi@spryker.com>
2024-11-11 16:58:19 +0100

    Wrap the whole check

--- bin/environment/mutagen-version-check.sh
+++ bin/environment/mutagen-version-check.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 function Environment::checkMutagenVersion() {
-    local mutagenInstalledVersion="$(Mutagen::getInstalledVersion)"
+    if [[ "${_PLATFORM}" == "macos" ]]; then
+        local mutagenInstalledVersion="$(Mutagen::getInstalledVersion)"
 
-    if [[ "${_PLATFORM}" == "macos" && ( "$mutagenInstalledVersion" == 0.18.* || "$mutagenInstalledVersion" > "0.18" ) ]]; then
-        Console::error "Error: Mutagen version 0.18.* is not supported yet. Please, run the command to install the latest supported version.
-brew unlink mutagen && brew unlink mutagen-compose && brew install mutagen-io/mutagen/mutagen@0.17 mutagen-io/mutagen/mutagen-compose@0.17"
-        exit 1
+        if [[ "$mutagenInstalledVersion" == 0.18.* || "$mutagenInstalledVersion" > "0.18" ]]; then
+            Console::error "Error: Mutagen version 0.18.* is not supported yet. Please, run the command to install the latest supported version.
+    brew unlink mutagen && brew unlink mutagen-compose && brew install mutagen-io/mutagen/mutagen@0.17 mutagen-io/mutagen/mutagen-compose@0.17"
+            exit 1
+        fi
     fi
 }
 
