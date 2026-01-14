e54c0aad4af5e2b8fd4de83532dd2d8f34a3a06f
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2026-01-14 05:52:32 +0100

    Update cli.sh

--- bin/standalone/cli.sh
+++ bin/standalone/cli.sh
@@ -43,19 +43,7 @@ function printLogo() {
 }
 
 function getEnvFile() {
-    local store="${APPLICATION_STORE:-$SPRYKER_CURRENT_REGION}"
-    if [ -n "${store}" ]; then
-        echo "${HOME}/env/$(echo "${store}" | tr '[:upper:]' '[:lower:]').env"
-        return
-    fi
-    for f in "${HOME}/env/"*.env; do
-        if [ -f "$f" ] && [[ ! "$f" =~ \.testing\.env$ ]]; then
-            echo "$f"
-            return
-        fi
-    done
-
-    echo "${HOME}/env/testing.env"
+    echo "${HOME}/env/$(echo "${APPLICATION_STORE:-$SPRYKER_CURRENT_REGION}" | tr '[:upper:]' '[:lower:]').env"
 }
 
 function setPrompt() {
